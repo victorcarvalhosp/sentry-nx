d6775941dc63cfda4dfa6fb404d0819408fce671
Victor Carvalho <victorcarvalhosp@gmail.com>
2024-09-25 14:22:22 -0300

    Downgrade Sentry

--- apps/sentry-nx/next.config.js
+++ apps/sentry-nx/next.config.js
@@ -1,7 +1,7 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
+const { withNx } = require('@nx/next');
 const { withSentryConfig } = require('@sentry/nextjs');
 
 /**
@@ -28,42 +28,6 @@ const nextConfig = {
   },
 };
 
-const sentryPlugin = (/** @type {any} */ passedConfig) =>
-  withSentryConfig(passedConfig, {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // org: 'webalys',
-    // project: 'sentry-nx-issue-test',
-
-    // // Only print logs for uploading source maps in CI
-    // silent: !process.env.CI,
-
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    // tunnelRoute: '/monitoring',
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    // disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    // automaticVercelMonitors: true,
-  });
-
 /**
  * @type {any[]}
  */
@@ -73,8 +37,6 @@ const plugins = [
   // sentryPlugin,
 ];
 
-// module.exports = composePlugins(...plugins)(nextConfig);
-
 module.exports = async (
   /** @type {string} */ phase,
   /** @type {any} */ context
@@ -82,10 +44,15 @@ module.exports = async (
   let updatedConfig = plugins.reduce((acc, fn) => fn(acc), nextConfig);
 
   updatedConfig = await withNx(updatedConfig)(phase, context);
-  updatedConfig = withSentryConfig(updatedConfig, {
-    widenClientFileUpload: true,
-    hideSourceMaps: true,
-  });
+
+  /** Use this if using Sentry 8 or higher */
+  // updatedConfig = withSentryConfig(updatedConfig, {
+  //   widenClientFileUpload: true,
+  //   hideSourceMaps: true,
+  // });
+
+  /** Use this if using Sentry 7 */
+  updatedConfig = withSentryConfig(updatedConfig);
 
   return updatedConfig;
 };
